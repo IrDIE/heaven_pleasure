bc39f57c00d3da47aa3343b668ff10e0fdacd6c8
Daria <43551406+sidsnow@users.noreply.github.com>
2025-09-08 22:51:10 +0300

    add ygpt

--- frontend-project/src/llm/config/code_review_workspace/test_student_code.py
+++
@@ -1,5 +0,0 @@
-# Код тестов для функции
-
-def test_answer_question_empty_input():
-    retriever = HybridRetriever(vs, bm25, docs)
-    assert answer_question('', retriever) == ('Не найдено в материалах.', [])
--- frontend-project/src/llm/config/settings.py
+++ frontend-project/src/llm/config/settings.py
@@ -1,37 +1,41 @@
 from pydantic_settings import BaseSettings
-from pydantic import BaseModel, Field
-from typing import List, Dict, Optional, Any
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY", None)
+FOLDER_ID = os.getenv("FOLDER_ID", None)
 
 
 class Config(BaseSettings):
     """Конфигурация для системы Code Review."""
-    
-    # LLM настройки
-    OLLAMA_BASE_URL: str = "http://192.168.31.49:11434/v1"
-    OLLAMA_API_KEY: str = "ollama"
-    
-    # Модели для разных задач
-    CODE_LLM_MODEL: str = "qwen2.5-coder"  # Для архитектуры и качества кода
-    GENERAL_LLM_MODEL: str = "qwen2.5"  # Для общего анализа
-    DATA_EXTRACTION_MODEL: str = "qwen2.5"  # Для тестировщика
-    
+
+    # OpenAI-совместимый endpoint для YandexGPT
+    OPENAI_COMPAT_BASE_URL: str = "https://llm.api.cloud.yandex.net/v1"
+
+    # Две модели: лёгкая и тяжёлая
+    BASE_LLM_MODEL: str = f"gpt://{FOLDER_ID}/yandexgpt-lite"
+
+    CODE_LLM_MODEL: str = f"gpt://{FOLDER_ID}/gpt-oss-120b/latest"
+
+    # Ключ
+    OLLAMA_API_KEY: str = API_KEY
+
     # Рабочая директория
     WORKSPACE_DIR: str = os.path.join(os.path.dirname(__file__), "code_review_workspace")
 
-    
-    # Настройки Docker для тестов
-    USE_DOCKER: bool = False  # Для code review обычно не нужен
-    DOCKER_IMAGE: Optional[str] = None
-    
-    # Стоимость токенов (опционально)
-    TOKEN_INPUT_COST: float = 0.0  # Можно установить 0 если не важно
+    # Настройки Docker
+    USE_DOCKER: bool = False
+    DOCKER_IMAGE: str | None = None
+
+    # Стоимость токенов
+    TOKEN_INPUT_COST: float = 0.0
     TOKEN_OUTPUT_COST: float = 0.0
-    
+
     # Настройки review
-    MAX_REVIEW_RETRIES: int = 3  # Попытки получить валидный ответ от агента
-    ENABLE_TEST_EXECUTION: bool = True  # Запускать ли тесты после создания
-    
+    MAX_REVIEW_RETRIES: int = 3
+    ENABLE_TEST_EXECUTION: bool = True
+
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
