cc09df53ce8f34720008bfe9103dab1069d1ac3f
Daria <43551406+sidsnow@users.noreply.github.com>
2025-09-09 16:06:48 +0300

    fix tools/runtime.sh, add tools/runtime.sh to frontend/runtime.sh

--- frontend-project/tools/runtime.sh
+++ frontend-project/tools/runtime.sh
@@ -1,6 +1,7 @@
-sudo apt-get update
-sudo apt-get install -y cppcheck openjdk-17-jdk wget git
+apt-get update
+apt-get wget npm -y
+apt-get install -y cppcheck openjdk-17-jdk git
 curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
-sudo apt-get install -y nodejs
-sudo npm install -g eslint@8 eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react htmlhint stylelint stylelint-config-standard
+apt-get install -y nodejs
+npm install -g eslint@8 eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react htmlhint stylelint stylelint-config-standard
 wget -q -O checkstyle-all.jar https://github.com/checkstyle/checkstyle/releases/download/checkstyle-10.12.4/checkstyle-10.12.4-all.jar
