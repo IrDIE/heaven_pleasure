3ce81767c7a2075f6fc3b6fbc5dc72735b8600f2
IrDIE <110756720+IrDIE@users.noreply.github.com>
2025-09-09 13:45:18 +0000

    refactored deps

--- frontend-project/runtime.sh
+++ frontend-project/runtime.sh
@@ -1,3 +1,13 @@
 cd frontend-project
-sh tools/runtime.sh
-python app.py
+pip install -r requirements.txt
+apt-get install wget
+apt install npm
+apt-get install -y cppcheck 
+# apt-get install -y  openjdk-17-jdk 
+apt-get install -y  git
+# curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+apt-get install -y nodejs
+npm install -g eslint
+# @8 eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react htmlhint stylelint stylelint-config-standard
+# wget -q -O checkstyle-all.jar https://github.com/checkstyle/checkstyle/releases/download/checkstyle-10.12.4/checkstyle-10.12.4-all.jar
+# python app.py
